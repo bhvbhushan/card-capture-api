e7396e3043454e0e4348ed53a151f0c07d104603
Bhavya Bhushan <bhvbhushan7@gmail.com>
2025-05-22 02:28:00 +0530

    [FIX] Add 5173 to allowed origins

--- app/config.py
+++ app/config.py
@@ -51,7 +51,9 @@ ALLOWED_ORIGINS = [
     "http://127.0.0.1:8086",
     "http://127.0.0.1:8087",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
 ]
 
 GEMINI_MODEL = " gemini-1.5-pro-latest"
