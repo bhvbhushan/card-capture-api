dd798ee6c9b22573181719f9669ab4598cc12651
Bhavya Bhushan <bhvbhushan7@gmail.com>
2025-05-27 01:27:52 +0530

    [FIX] Fix trailing /

--- app/config.py
+++ app/config.py
@@ -54,7 +54,7 @@ ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://gen-lang-client-0493571343.web.app/'
+    'https://gen-lang-client-0493571343.web.app'
 ]
 
 GEMINI_MODEL = " gemini-1.5-pro-latest"
